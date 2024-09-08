03eff6ef8bae3f6e782cba6acecf8ab2bcc335f1
xredportx <ericzabala634@gmail.com>
2024-09-08 11:02:57 -0400

    update buttons
    Changed the top bar buttons to have the active button stay black when clicked to show they are active. Also, changed it when a user loads in the home button is "active".

--- js/page-navigation.js
+++ js/page-navigation.js
@@ -1,31 +1,48 @@
+const home = document.getElementById('home-page');
+const about = document.getElementById('about-page');
+const skill = document.getElementById('Skills-Page');
+const contact= document.getElementById('Contact-page');
+
+const btnContainer = document.getElementById("Desktop-buttons");
+var btns = btnContainer.getElementsByClassName("desktop-button");
+
 function homeSection() {
     document.body.className = 'home-bg';
-    document.getElementById('home-page').style.display = 'block';
-    document.getElementById('about-page').style.display = 'none';
-    document.getElementById('Skills-Page').style.display = 'none';
-    document.getElementById('Contact-page').style.display = 'none';
+    home.style.display = 'block';
+    about.style.display = 'none';
+    skill.style.display = 'none';
+    contact.style.display = 'none';
 }
 
 function aboutSection() {
     document.body.className = 'about-bg';
-    document.getElementById('home-page').style.display = 'none';
-    document.getElementById('about-page').style.display = 'block';
-    document.getElementById('Skills-Page').style.display = 'none';
-    document.getElementById('Contact-page').style.display = 'none';
+    home.style.display = 'none';
+    about.style.display = 'block';
+    skill.style.display = 'none';
+    contact.style.display = 'none';
 }
 
 function skillsSection() {
     document.body.className = 'skills-bg';
-    document.getElementById('home-page').style.display = 'none';
-    document.getElementById('about-page').style.display = 'none';
-    document.getElementById('Skills-Page').style.display = 'block';
-    document.getElementById('Contact-page').style.display = 'none';
+    home.style.display = 'none';
+    about.style.display = 'none';
+    skill.style.display = 'block';
+    contact.style.display = 'none';
 }
 
 function contactSection() {
     document.body.className = 'contact-bg';
-    document.getElementById('home-page').style.display = 'none';
-    document.getElementById('about-page').style.display = 'none';
-    document.getElementById('Skills-Page').style.display = 'none';
-    document.getElementById('Contact-page').style.display = 'block';
+    home.style.display = 'none';
+    about.style.display = 'none';
+    skill.style.display = 'none';
+    contact.style.display = 'block';
+}
+
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
 }
